7cd3ea40c0323f5cbf9fa0a609a259c60e962161
Felix Hagemann <30291312+fhagemann@users.noreply.github.com>
2020-10-23 14:30:18 +0200

    refactor plot recipes for GR support in docs

--- src/Geometries/VolumePrimitives/plot_recipes.jl
+++ src/Geometries/VolumePrimitives/plot_recipes.jl
@@ -1,28 +1,25 @@
-function LineSegments(t::SolidStateDetectors.Tube{T}; n::Int = 30)::Vector{SolidStateDetectors.LineSegment{T, 3, :cartesian}} where {T <: SolidStateDetectors.SSDFloat}
-    ls = SolidStateDetectors.LineSegment{T, 3, :cartesian}[]
-    φs = range(t.φ_interval.left, length = n+1, stop = t.φ_interval.right)
+function LineSegments(t::Tube{T})::Vector{AbstractLine{T,3,:cartesian}} where {T <: SSDFloat}
+    ls = AbstractLine{T, 3, :cartesian}[]
+    translate::CartesianVector{T} = ismissing(t.translate) ? CartesianVector{T}([0, 0, 0]) : t.translate
     for r in (t.r_interval.left == 0 ? [t.r_interval.right] : [t.r_interval.left, t.r_interval.right])
         for z in [t.z_interval.left, t.z_interval.right]
-            pts = [CartesianPoint{T}(r * sin(φ), r * cos(φ), z) for φ in φs]
-            for i in 1:n
-                push!(ls, SolidStateDetectors.LineSegment(pts[i], pts[i+1]))
-            end
+            push!(ls, PartialCircle(r, t.φ_interval.left, t.φ_interval.right, translate + CartesianVector{T}([0, 0, z])))
         end
     end
     for r in [t.r_interval.left, t.r_interval.right]
         if r != 0
-            for φ in ((φs[end] - φs[1] ≈ 2π) ? [φs[1]] : [φs[1], φs[end]])
-                push!(ls, SolidStateDetectors.LineSegment(
-                    CartesianPoint{T}(r * sin(φ), r * cos(φ), t.z_interval.left),
-                    CartesianPoint{T}(r * sin(φ), r * cos(φ), t.z_interval.right)))
+            for φ in ((t.φ_interval.right - t.φ_interval.left ≈ 2π) ? [t.φ_interval.left] : [t.φ_interval.left, t.φ_interval.right])
+                push!(ls, LineSegment(
+                    CartesianPoint{T}(r * cos(φ), r * sin(φ), t.z_interval.left) + translate,
+                    CartesianPoint{T}(r * cos(φ), r * sin(φ), t.z_interval.right) + translate))
             end
         end
     end
-    for φ in ((φs[end] - φs[1] ≈ 2π) ? [] : [φs[1], φs[end]])
+    for φ in ((t.φ_interval.right - t.φ_interval.left ≈ 2π) ? [] : [t.φ_interval.left, t.φ_interval.right])
         for z in [t.z_interval.left, t.z_interval.right]
-            push!(ls, SolidStateDetectors.LineSegment(
-                CartesianPoint{T}(t.r_interval.left * sin(φ),  t.r_interval.left * cos(φ),  z),
-                CartesianPoint{T}(t.r_interval.right * sin(φ), t.r_interval.right * cos(φ), z)))
+            push!(ls, LineSegment(
+                CartesianPoint{T}(t.r_interval.left * cos(φ), t.r_interval.left * sin(φ), z) + translate,
+                CartesianPoint{T}(t.r_interval.right * cos(φ), t.r_interval.right * sin(φ), z) + translate))
         end
     end
     return ls
@@ -30,6 +27,7 @@ end
 
 @recipe function f(t::Tube{T}; n = 30, seriescolor = :green) where {T}
     linewidth --> 2
+    n --> n
     @series begin
         seriescolor --> seriescolor
         label --> "Tube"
@@ -37,51 +35,42 @@ end
     end
     label := ""
     seriescolor := seriescolor
-    LineSegments(t, n = n)
+    LineSegments(t)
 end
 
 
-
-function LineSegments(c::SolidStateDetectors.Cone{T}; n::Int = 30)::Vector{SolidStateDetectors.LineSegment{T, 3, :cartesian}} where {T <: SolidStateDetectors.SSDFloat}
-    ls = SolidStateDetectors.LineSegment{T, 3, :cartesian}[]
-    φs = range(c.φStart, length = n+1, stop = c.φStop)
+function LineSegments(c::Cone{T})::Vector{AbstractLine{T, 3, :cartesian}} where {T <: SSDFloat}
+    ls = AbstractLine{T, 3, :cartesian}[]
+    translate::CartesianVector{T} = ismissing(c.translate) ? CartesianVector{T}([0, 0, 0]) : c.translate
     for r in [c.rStart1, c.rStop1]
-        for z in [c.zStart]
-            pts = [CartesianPoint{T}(r * sin(φ), r * cos(φ), z) for φ in φs]
-            for i in 1:n
-                push!(ls, SolidStateDetectors.LineSegment(pts[i], pts[i+1]))
-            end
-        end
+        push!(ls, PartialCircle(r, c.φStart, c.φStop, translate + CartesianVector{T}([0, 0, c.zStart]))) 
     end
     for r in [c.rStart2, c.rStop2]
-        for z in [c.zStop]
-            pts = [CartesianPoint{T}(r * sin(φ), r * cos(φ), z) for φ in φs]
-            for i in 1:n
-                push!(ls, SolidStateDetectors.LineSegment(pts[i], pts[i+1]))
-            end
-        end
+        push!(ls, PartialCircle(r, c.φStart, c.φStop, translate + CartesianVector{T}([0, 0, c.zStop])))
+
     end
-    for φ in ((φs[end] - φs[1] ≈ 2π) ? [φs[1]] : [φs[1], φs[end]])
-        push!(ls, SolidStateDetectors.LineSegment(
-            CartesianPoint{T}(c.rStart1 * sin(φ), c.rStart1 * cos(φ), c.zStart),
-            CartesianPoint{T}(c.rStart2 * sin(φ), c.rStart2 * cos(φ), c.zStop)))
-        push!(ls, SolidStateDetectors.LineSegment(
-            CartesianPoint{T}(c.rStop1 * sin(φ), c.rStop1 * cos(φ), c.zStart),
-            CartesianPoint{T}(c.rStop2 * sin(φ), c.rStop2 * cos(φ), c.zStop)))
+    for φ in ((c.φStop - c.φStart ≈ 2π) ? [c.φStart] : [c.φStart, c.φStop])
+        push!(ls, LineSegment(
+            CartesianPoint{T}(c.rStart1 * sin(φ), c.rStart1 * cos(φ), c.zStart) + translate,
+            CartesianPoint{T}(c.rStart2 * sin(φ), c.rStart2 * cos(φ), c.zStop) + translate))
+        push!(ls, LineSegment(
+            CartesianPoint{T}(c.rStop1 * sin(φ), c.rStop1 * cos(φ), c.zStart) + translate,
+            CartesianPoint{T}(c.rStop2 * sin(φ), c.rStop2 * cos(φ), c.zStop) + translate))
     end
-    for φ in ((φs[end] - φs[1] ≈ 2π) ? [φs[1]] : [φs[1], φs[end]])
-        push!(ls, SolidStateDetectors.LineSegment(
-            CartesianPoint{T}(c.rStart1 * sin(φ),  c.rStart1 * cos(φ),  c.zStart),
-            CartesianPoint{T}(c.rStop1 * sin(φ),   c.rStop1 * cos(φ), c.zStart)))
-        push!(ls, SolidStateDetectors.LineSegment(
-            CartesianPoint{T}(c.rStart2 * sin(φ),  c.rStart2 * cos(φ),  c.zStop),
-            CartesianPoint{T}(c.rStop2 * sin(φ),   c.rStop2 * cos(φ), c.zStop)))
+    for φ in ((c.φStop - c.φStart ≈ 2π) ? [c.φStart] : [c.φStart, c.φStop])
+        push!(ls, LineSegment(
+            CartesianPoint{T}(c.rStart1 * sin(φ),  c.rStart1 * cos(φ),  c.zStart) + translate,
+            CartesianPoint{T}(c.rStop1 * sin(φ),   c.rStop1 * cos(φ), c.zStart) + translate))
+        push!(ls, LineSegment(
+            CartesianPoint{T}(c.rStart2 * sin(φ),  c.rStart2 * cos(φ),  c.zStop) + translate,
+            CartesianPoint{T}(c.rStop2 * sin(φ),   c.rStop2 * cos(φ), c.zStop) + translate))
     end
     return ls
 end
 
 @recipe function f(c::Cone{T}; n = 30, seriescolor = :orange) where {T}
     linewidth --> 2
+    n --> n
     @series begin
         seriescolor --> seriescolor
         label --> "Cone"
@@ -89,9 +78,8 @@ end
     end
     seriescolor := seriescolor
     label := ""
-    LineSegments(c, n = n)
+    LineSegments(c)
 end
-
 
 #=
 @recipe function f(Vol::Tube{T}, coloring = missing) where T <: SSDFloat
